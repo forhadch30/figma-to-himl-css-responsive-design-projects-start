9ab6dac3a5de192f9cc8e010051c311220d4227f
forhad30 <forhadchariani30@gmail.com>
2024-11-28 17:15:54 +0600

    figma to html css design home page done

---
+++ home.js
@@ -0,0 +1,5 @@
+var typed = new Typed('.primary', {
+    strings: ['plan is totally independent of the WKR and is free for you as an employer. The employee pays the lease costs and you, as the employer, decide whether you want to contribute to the costs.'],
+    typeSpeed: 80,
+    backSpeed: 80,
+});
