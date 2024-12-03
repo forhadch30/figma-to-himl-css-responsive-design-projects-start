d6160b8f8943949591807860fac9c3f0ac0c8eaa
forhad30 <forhadchariani30@gmail.com>
2024-12-03 18:40:45 +0600

    image done

--- home.js
+++ home.js
@@ -1,5 +1,6 @@
-var typed = new Typed('.primary', {
-    strings: ['plan is totally independent of the WKR and is free for you as an employer. The employee pays the lease costs and you, as the employer, decide whether you want to contribute to the costs.'],
-    typeSpeed: 80,
-    backSpeed: 80,
+var typed = new Typed('.text-red-600', {
+    strings: ['Web Development', 'Theme Design', 'Figma To Responsive Design', 'products', ''],
+    typeSpeed: 60,
+    backSpeed: 100,
+    loop: true
 });
