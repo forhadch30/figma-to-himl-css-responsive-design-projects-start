b53f11588928847d01ec5ec31fb860f9ba956a73
forhad30 <forhadchariani30@gmail.com>
2024-12-05 10:33:03 +0600

    all link done

--- home.js
+++ home.js
@@ -1,4 +1,4 @@
-var typed = new Typed('.text-red-600', {
+var typed = new Typed('.text-orange-500', {
     strings: ['Web Development', 'Theme Design', 'Figma To Responsive Design', 'products', ''],
     typeSpeed: 60,
     backSpeed: 100,
