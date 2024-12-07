9ee70cf629589591785d25564bbccb3dabb7923c
forhad30 <forhadchariani30@gmail.com>
2024-12-07 18:01:30 +0600

    all section done

--- home.js
+++ home.js
@@ -1,5 +1,5 @@
-var typed = new Typed('.text-orange-500', {
-    strings: ['Web Development', 'Theme Design', 'Figma To Responsive Design', 'products', ''],
+const typed = new Typed('.text-orange-500', {
+    strings: ['Web Development', 'Theme Designer', 'Figma To Responsive Design', 'products', ''],
     typeSpeed: 60,
     backSpeed: 100,
     loop: true
